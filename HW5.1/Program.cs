175fce3245ddbc2280e32f9ddb2d8e7d0876f5dd
Ефрем Шпаков <ev.shpakov@mail.ru>
2021-05-31 21:32:49 +0300

    CW2

--- HW5.1/Program.cs
+++ HW5.1/Program.cs
@@ -7,6 +7,7 @@ namespace HW5._1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("ello World!");
         }
     }
 }
